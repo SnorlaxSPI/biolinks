399335f045203a123ff5f5e65413cd4fa57bbe5f
SnorlaxSPI <aflinsjr@icloud.com>
2025-02-20 18:23:52 -0300

    Criando nosso controller - completo

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloController;
 use App\Http\Controllers\WelcomeController;
 
 Route::get('/', WelcomeController::class);
 Route::get('/hello', [HelloController::class, 'show']);
-Route::get('/login', function () {
-  return 'login';
-});
+Route::get('/login', [LoginController::class, 'index']);
