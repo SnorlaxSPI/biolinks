cf1587916c638cbb8d1414c593cb628a5a850aa6
Alessandro L <alessandro.lins@gruponewway.com.br>
2025-02-25 21:47:14 -0300

    Validação de formulário

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
@@ -16,16 +16,22 @@ class LoginController extends Controller
     public function login()
     {
         //dd(request()->email);
+        request()->validate([
+            'email' => 'required', 'email',
+            'password' => 'required'
+        ]);
 
         if($user = User::query()
         ->where('email', '=', request()->email)
         ->first()) {
 
+        if (hash::check(request()->password, $user->password)) {
         auth()->login($user);
 
         return to_route('dashboard');
         }
+    }
 
-        return back()->with(['message' => 'Invalid credentials']);
+        return back()->with(['message' => 'Não deu certo']);
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,31 @@
 <div>
     <h1>Login</h1>
 
+    @if ($message = session()->get('message'))
+    <div>{{ $message }} </div>
+    @endif
+
     <form action="/login" method="post">
         @csrf
-        <input name="email" type="email" placeholder="Email">
-        <input name="password" type="password" placeholder="Password">
+
+        <div>
+            <input name="email" placeholder="Email" value="{{ old('email')}}">
+            @error('email')
+                <span> {{ $message }} </span>
+            @enderror
+        </div>
+
+        <br>
+
+        <div>
+            <input name="password" type="password" placeholder="Senha">
+            @error('password')
+                <span> {{ $message }} </span>
+            @enderror
+        </div>
+
+        <br>
+
         <button>Logar</button>
     </form>
 
